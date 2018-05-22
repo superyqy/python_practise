9a696782cd180c624e4e0c852220a1ea0547801a
yqy <690038031@qq.com>
2018-05-22 08:57:41 -0700

    ADD excel handler

---
+++ PublicUtility/auto_mail_sender/run_email_server.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+pid=`ps -ef |grep "python ./mail_sender.py"|grep -v grep |awk '{print $2}'`
+if [ "a$pid" != "a" ]; then
+  #kill -9 $pid
+  echo 'this process already running'
+  exit 1
+fi
+
+nohup python ./mail_sender.py > mapil_server_outut.log 2>&1 &
